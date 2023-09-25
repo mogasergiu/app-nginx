e63ec07ed800b65e9b0bcc76517de77a2b518131
Sergiu Moga <sergiu@unikraft.io>
2023-09-25 20:21:49 +0300

    test

---
+++ launch-x86.sh
@@ -0,0 +1,11 @@
+sudo qemu-system-x86_64 \
+                        -fsdev local,id=myid,path=$(pwd)/fs0,security_model=none \
+                        -device virtio-9p-pci,fsdev=myid,mount_tag=fs0,disable-modern=on,disable-legacy=off \
+                        -netdev bridge,id=en0,br=virbr0 \
+                        -device virtio-net-pci,netdev=en0 \
+                        -drive file=nginx.ext,if=none,id=drive0,format=raw \
+                        -device virtio-blk-pci,drive=drive0,id=bd0 \
+                        -kernel "build/app-nginx_qemu-x86_64" \
+                        -append "netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 netdev.ipv4_subnet_mask=255.255.255.0 vfs.fstab=[ "bd0:/ext:ext2" ] -- -c /ext/nginx/conf/nginx.conf" \
+                        -nographic \
+                        -enable-kvm -cpu host -initrd nginx.initrd
